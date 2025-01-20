cb95c35e3e751a9cefe4ba938e957f88db8ad8c2
Daniel Lochner <daniel@daniellochner.com>
2025-01-20 19:16:54 +0200

    Update to 1.4.8

--- Assets/Creature Creator SDK/Internal/Scripts/Editor/ProjectInit.cs
+++ Assets/Creature Creator SDK/Internal/Scripts/Editor/ProjectInit.cs
@@ -10,7 +10,7 @@ public static class ProjectInit
 	// yes, you can just change this value. but I recommend you don't.
 	// some features of the map may break and not work correctly, depending
 	// on the changes I made between versions.
-	public const string SDKVersion = "1.0.0";
+	public const string SDKVersion = "1.4.8";
 	const string requiredVersion = "2021.3.42f1";
 	const string configURL = "https://playcreature.com/sdk/config.json";
 
