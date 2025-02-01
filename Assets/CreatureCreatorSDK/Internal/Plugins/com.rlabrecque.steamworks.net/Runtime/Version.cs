9c25ef171722a1be73971a1a25627460f3538854
Daniel Lochner <daniel@daniellochner.com>
2025-02-01 13:18:52 +0200

    Remove DISABLESTEAMWORKS definitions

--- Assets/CreatureCreatorSDK/Internal/Plugins/com.rlabrecque.steamworks.net/Runtime/Version.cs
+++ Assets/CreatureCreatorSDK/Internal/Plugins/com.rlabrecque.steamworks.net/Runtime/Version.cs
@@ -6,7 +6,7 @@
 // Changes to this file will be reverted when you update Steamworks.NET
 
 #if !(UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_STANDALONE_OSX || STEAMWORKS_WIN || STEAMWORKS_LIN_OSX)
-#define DISABLESTEAMWORKS
+
 #endif
 
 #if !DISABLESTEAMWORKS
