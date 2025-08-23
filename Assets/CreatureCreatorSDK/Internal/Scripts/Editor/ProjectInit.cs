77b83d5f76d2e262c14200b230eddd6c4859d911
Daniel Lochner <daniel@daniellochner.com>
2025-08-23 16:12:52 +0200

    Update ProjectInit.cs

--- Assets/CreatureCreatorSDK/Internal/Scripts/Editor/ProjectInit.cs
+++ Assets/CreatureCreatorSDK/Internal/Scripts/Editor/ProjectInit.cs
@@ -7,7 +7,7 @@ using UnityEngine.Networking;
 [InitializeOnLoad]
 public static class ProjectInit
 {
-	public const string SDKVersion = "1.6.19";
+	public const string SDKVersion = "1.6.20";
 	const string requiredVersion = "6000.1.12f1";
 	const string configURL = "https://playcreature.com/sdk/config.json";
 
