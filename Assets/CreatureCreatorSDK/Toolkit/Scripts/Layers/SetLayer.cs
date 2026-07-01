35e6215c73ea332335ac645d474f550bb0029a6d
Daniel Lochner <daniel@daniellochner.com>
2026-06-30 21:05:08 -0400

    Update SetLayer.cs

--- Assets/CreatureCreatorSDK/Toolkit/Scripts/Layers/SetLayer.cs
+++ Assets/CreatureCreatorSDK/Toolkit/Scripts/Layers/SetLayer.cs
@@ -1,9 +1,8 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace DanielLochner.CreatureCrafter.SDK
 {
-    public class SetLayer : ErrorCollectionBehaviour
+    public class SetLayer : MonoBehaviour
     {
         public bool includeChildren;
         public LayerType layerType;
@@ -15,18 +14,6 @@ namespace DanielLochner.CreatureCrafter.SDK
             Ground,
             UI,
             PostProcessing
-        }
-
-        public override void RunErrorChecks(ref List<string> errors)
-        {
-            base.RunErrorChecks(ref errors);
-
-#if UNITY_NAVIGATION
-            if (layerType == LayerType.Ground && GetComponentInChildren<Unity.AI.Navigation.NavMeshSurface>() == null)
-            {
-                errors.Add("Ground layers should probably have a NavMeshSurface component attached and baked.");
-            }
-#endif
         }
     }
 }
